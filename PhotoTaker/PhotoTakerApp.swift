cd91edabdc6ce750c0df58bd444a163775be30a5
Denis Haritonenko <114603705+zassimouse@users.noreply.github.com>
2024-10-25 16:22:25 +0300

    Initial Commit

---
+++ PhotoTaker/PhotoTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTakerApp.swift
+//  PhotoTaker
+//
+//  Created by Denis Haritonenko on 25.10.24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
